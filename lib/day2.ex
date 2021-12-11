3c03bfa6ae3a04765050077dabb5f1b2f011a145
Maxb <bittmanmax@gmail.com>
2021-12-10 19:33:46 -0800

    Code reorg

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,79 +1,13 @@
-defmodule Aoc2021 do
-  @moduledoc """
-  Documentation for `Aoc2021`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc2021.day()
-      1215
-
-  """
-
-  def number_stream do
-    filestream = File.stream!("data/day1-input1")
-    replaced = Stream.map(filestream, fn x -> String.replace(x, "\n", "") end)
-
-    numbered =
-      Stream.map(replaced, fn x -> Integer.parse(x) end)
-      |> Stream.filter(fn x -> tuple_size(x) == 2 end)
-      |> Stream.map(fn x -> elem(x, 0) end)
-
-    numbered
-  end
-
-  def day1_part1 do
-    numbered = number_stream()
-    zipped = Stream.zip(numbered, Stream.drop(numbered, 1))
-
-    count =
-      Enum.reduce(zipped, 0, fn x, acc ->
-        if elem(x, 1) > elem(x, 0) do
-          acc + 1
-        else
-          acc
-        end
-      end)
-
-    count
-  end
-
-  def day1_part2 do
-    numbered = number_stream()
-
-    triplets =
-      Stream.zip(numbered, Stream.drop(numbered, 1))
-      |> Stream.zip(Stream.drop(numbered, 2))
-      |> Stream.map(fn x -> [elem(elem(x, 0), 0), elem(elem(x, 0), 1), elem(x, 1)] end)
-
-    triplets_summed = Stream.map(triplets, fn [x, y, z] -> x + y + z end)
-
-    zipped = Stream.zip(triplets_summed, Stream.drop(triplets_summed, 1))
-
-    count =
-      Enum.reduce(zipped, 0, fn x, acc ->
-        if elem(x, 1) > elem(x, 0) do
-          acc + 1
-        else
-          acc
-        end
-      end)
-
-    count
-  end
-
-  def day2_commands do
+defmodule Day2 do
+  def commands do
     filestream = File.stream!("data/day2-input1")
 
     Stream.map(filestream, fn x -> String.replace(x, "\n", "") end)
     |> Stream.map(fn x -> String.split(x, " ") end)
   end
 
-  def day2_part1 do
-    commands = day2_commands()
+  def part1 do
+    commands = commands()
 
     pos =
       Enum.reduce(
@@ -98,8 +32,8 @@ defmodule Aoc2021 do
     pos[:forward] * pos[:depth]
   end
 
-  def day2_part2 do
-    commands = day2_commands()
+  def part2 do
+    commands = commands()
 
     pos =
       Enum.reduce(
--- test/aoc2021_test.exs
+++ test/aoc2021_test.exs
@@ -1,20 +1,19 @@
 defmodule Aoc2021Test do
   use ExUnit.Case
-  doctest Aoc2021
 
   test "day 1 part 1" do
-    assert Aoc2021.day1_part1() == 1215
+    assert Day1.part1() == 1215
   end
 
   test "day 1 part 2" do
-    assert Aoc2021.day1_part2() == 1150
+    assert Day1.part2() == 1150
   end
 
   test "day 2 part 1" do
-    assert Aoc2021.day2_part1() == 1_813_801
+    assert Day2.part1() == 1_813_801
   end
 
   test "day 2 part 2" do
-    assert Aoc2021.day2_part2() == 1_960_569_556
+    assert Day2.part2() == 1_960_569_556
   end
 end
