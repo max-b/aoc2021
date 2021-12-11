df6710f52d0d89a2e9874c3d43c035996703e611
Maxb <bittmanmax@gmail.com>
2021-12-10 19:09:52 -0800

    Day 2

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -64,4 +64,68 @@ defmodule Aoc2021 do
 
     count
   end
+
+  def day2_commands do
+    filestream = File.stream!("data/day2-input1")
+
+    Stream.map(filestream, fn x -> String.replace(x, "\n", "") end)
+    |> Stream.map(fn x -> String.split(x, " ") end)
+  end
+
+  def day2_part1 do
+    commands = day2_commands()
+
+    pos =
+      Enum.reduce(
+        commands,
+        [{:forward, 0}, {:depth, 0}],
+        fn command, acc ->
+          [direction, magnitude_string] = command
+          [{:forward, forward}, {:depth, depth}] = acc
+          magnitude = elem(Integer.parse(magnitude_string), 0)
+
+          newpos =
+            case direction do
+              "forward" -> [{:forward, forward + magnitude}, {:depth, depth}]
+              "down" -> [{:forward, forward}, {:depth, depth + magnitude}]
+              "up" -> [{:forward, forward}, {:depth, depth - magnitude}]
+            end
+
+          newpos
+        end
+      )
+
+    pos[:forward] * pos[:depth]
+  end
+
+  def day2_part2 do
+    commands = day2_commands()
+
+    pos =
+      Enum.reduce(
+        commands,
+        [{:forward, 0}, {:depth, 0}, {:aim, 0}],
+        fn command, acc ->
+          [direction, magnitude_string] = command
+          [{:forward, forward}, {:depth, depth}, {:aim, aim}] = acc
+          magnitude = elem(Integer.parse(magnitude_string), 0)
+
+          newpos =
+            case direction do
+              "down" ->
+                [{:forward, forward}, {:depth, depth}, {:aim, aim + magnitude}]
+
+              "up" ->
+                [{:forward, forward}, {:depth, depth}, {:aim, aim - magnitude}]
+
+              "forward" ->
+                [{:forward, forward + magnitude}, {:depth, depth + aim * magnitude}, {:aim, aim}]
+            end
+
+          newpos
+        end
+      )
+
+    pos[:forward] * pos[:depth]
+  end
 end
--- test/aoc2021_test.exs
+++ test/aoc2021_test.exs
@@ -9,4 +9,12 @@ defmodule Aoc2021Test do
   test "day 1 part 2" do
     assert Aoc2021.day1_part2() == 1150
   end
+
+  test "day 2 part 1" do
+    assert Aoc2021.day2_part1() == 1_813_801
+  end
+
+  test "day 2 part 2" do
+    assert Aoc2021.day2_part2() == 1_960_569_556
+  end
 end
